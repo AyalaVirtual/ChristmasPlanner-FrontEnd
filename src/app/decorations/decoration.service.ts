1d289fd1743ccba83f1e8ec3bfc65419cf0e145d
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-23 14:55:41 -0400

    Edit decoration service file.

--- src/app/decorations/decoration.service.ts
+++ src/app/decorations/decoration.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 // Any time HTTP is used, this must be imported 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 // This means the app is already aware of it, so it's already available 
@@ -8,7 +8,15 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DecorationService {
-    private apiUrl : string = 'http://localhost:9092/api'
+    // This variable points to the Spring Boot API's URL and is where the Angular front-end will send HTTP requests to interact with the back-end 
+    private apiUrl : string = 'http://localhost:9092/api';
+
+    httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
 
   constructor(private http : HttpClient) { }
 
@@ -19,8 +27,10 @@ export class DecorationService {
   getDecorationById(decorationId : number) {
     return this.http.get(`${this.apiUrl}/decorations/{decorationId}`);
   }
+  
 
   createDecoration(decoration : any) {
+
     return this.http.post(`${this.apiUrl}/decorations`, decoration);
   }
 
