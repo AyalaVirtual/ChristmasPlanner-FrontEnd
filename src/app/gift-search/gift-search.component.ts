86968049ea5ca1ee798d6097017ef1053fa50eb8
AyalaVirtual <erica.ayala.work@gmail.com>
2023-10-25 18:33:39 -0400

    Add logic to gift search component to try to get search function working. Not successful as of yet. Comment out search component in HTML of all models since it's nonfunctional.

--- src/app/gift-search/gift-search.component.ts
+++ src/app/gift-search/gift-search.component.ts
@@ -2,7 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { GiftService } from '../gifts/gift.service';
-import { Gift } from '../gift-page/gift-page.component';
+
+
+
+// ADDED FOR SEARCH 
+export type Gift = {
+  id : number;
+  name : String;
+  description : String;
+  tag : String;
+ }
 
 
 @Component({
@@ -11,8 +20,10 @@ import { Gift } from '../gift-page/gift-page.component';
   styleUrls: ['./gift-search.component.css']
 })
 export class GiftSearchComponent implements OnInit {
-  input : string = '';
-  gift : any = '';
+  // This property holds the user's search input 
+  searchTerm: string = '';
+  gifts : Gift[] = [];
+  gift : any;
 
   // This is the observable 
   searchSubject = new Subject();
@@ -21,20 +32,20 @@ export class GiftSearchComponent implements OnInit {
   constructor(private giftService : GiftService) {}
 
 
-  findGiftByTag(input : string) : void {
+  findGiftByTag(searchTerm : string) {
     // This publishes that something happened (in this case, the event of the findGift() method firing, or receiving the input type)  
-    this.searchSubject.next(input);
+    this.searchSubject.next(searchTerm);
 
-    /* if (this.gift.tag.includes(this.searchSubject.next(input))) {
+    if (this.gift.tag.includes(this.searchSubject.next(searchTerm))) {
           return this.gift;
-    } */
+    }
   }
 
   ngOnInit() : void {
     this.searchSubject
     // this is like a setTimeout() method that only sends another request if you search for something different than the previous search 
-    .pipe(debounceTime(1000), distinctUntilChanged())
-    .subscribe(input => {
+    // .pipe(debounceTime(1000), distinctUntilChanged())
+    .subscribe(searchTerm => {
       this.giftService.getAllGifts()
       // This means it's listening in for the response 
         .subscribe(response => {
@@ -43,6 +54,15 @@ export class GiftSearchComponent implements OnInit {
           this.gift = response 
         })
     })
+  }
+
+
+  // ADDED FOR SEARCH BAR 
+  // Populate the 'items' array with your data, e.g., from a service or API.
+  search(): void {
+    if (this.searchTerm === this.gift.tag)
+
+    this.gifts = this.gifts.filter(gift => gift.tag.toLowerCase().includes(this.searchTerm.toLowerCase()));
   }
 
 }
