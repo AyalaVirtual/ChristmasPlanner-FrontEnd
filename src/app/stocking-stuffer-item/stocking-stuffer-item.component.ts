4bba08beb58461363c2a46264f732a488089732d
Erica Ayala <erica.ayala.work@gmail.com>
2023-11-23 23:15:33 -0500

    Add comments to stocking-stuffer-item.component.ts

--- src/app/stocking-stuffer-item/stocking-stuffer-item.component.ts
+++ src/app/stocking-stuffer-item/stocking-stuffer-item.component.ts
@@ -21,17 +21,19 @@ export class StockingStufferItemComponent implements OnInit {
   stockingStufferItem : any;
 
 
-  // Passing in ActivatedRoute makes the URL routes available to the class
+  // ActivatedRoute is a service that provides information about the currently activated route. Passing in ActivatedRoute as a parameter makes the URL routes available to the class. 
   constructor(private stockingStufferItemService : StockingStufferItemService, private route: ActivatedRoute) {}
 
   
+  /*
+    This retrieves the ID from the URL route parameters using the "snapshot" of the ActivatedRoute service. It then checks if the ID is not empty. If it is not empty, it calls the "getStockingStufferById" method of the "stockingStufferItemService" service, passing the ID as a parameter. This method returns an observable that emits the stocking stuffer item data. It then uses the "subscribe" method on the observable to handle the emitted stocking stuffer item data and stores it in the "stockingStufferItem" property of the component.
+   */
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get('id') || '';
-    // This checks if stocking stuffer id is not null or undefined before making the HTTP request 
+    
     if (id) {
       this.stockingStufferItemService.getStockingStufferById(+id)
         .subscribe((stockingStufferItem) => {
-          // This assigns the retrieved data to the component property 
           this.stockingStufferItem = stockingStufferItem.data;
       });
     }
